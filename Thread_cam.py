ea76b8328b4b35892768747c266fbd101837d2c1
LouisThanatos02 <jimmy963909827@gmail.com>
2021-01-30 17:30:48 +0800

    成功完成針對相機的讀取與顯示分成2個不同的線程

--- Thread_cam.py
+++ Thread_cam.py
@@ -13,6 +13,7 @@
 
 import cv2
 import numpy as np
+import threading
 from csi_camera import CSI_Camera
 
 show_fps = True
@@ -21,7 +22,7 @@ show_fps = True
 def draw_label(cv_image, label_text, label_position):
     font_face = cv2.FONT_HERSHEY_SIMPLEX
     scale = 0.5
-    color = (255,255,255)
+    color = (0,255,0)
     # You can get the size of the string with cv2.getTextSize here
     cv2.putText(cv_image, label_text, label_position, font_face, scale, color, 1, cv2.LINE_AA)
 
@@ -29,17 +30,26 @@ def draw_label(cv_image, label_text, label_position):
 # Return an image
 def read_camera(csi_camera,display_fps):
     _ , camera_image=csi_camera.read()
+    thread_name=[]
+    thread_name=threading.enumerate()
     if display_fps:
-        draw_label(camera_image, "Frames Displayed (PS): "+str(csi_camera.last_frames_displayed),(10,20))
-        draw_label(camera_image, "Frames Read (PS): "+str(csi_camera.last_frames_read),(10,40))
+        draw_label(camera_image, "FDPS: "+str(csi_camera.last_frames_displayed),(10,20))
+        draw_label(camera_image, "FRPS: "+str(csi_camera.last_frames_read),(10,40))
+        draw_label(camera_image,"Thread: "+str(threading.active_count()),(10,60))
+        for i in range(threading.active_count()):
+            draw_label(camera_image,"Thread%d_name: "%(i+1)+str(thread_name[i]),(10,60+((i+1)*20)))
     return camera_image
 
 # Good for 1280x720
 # DISPLAY_WIDTH=640
 # DISPLAY_HEIGHT=360
 # For 1920x1080
+CAPTURE_WIDTH=2880
+CAPTURE_HIEGHT=1620
 DISPLAY_WIDTH=1920
 DISPLAY_HEIGHT=1080
+FARMERATE=30
+
 
 # 1920x1080, 30 fps
 SENSOR_MODE_1080=2
@@ -47,35 +57,22 @@ SENSOR_MODE_1080=2
 SENSOR_MODE_720=3
 
 def start_cameras():
-    left_camera = CSI_Camera()
-    left_camera.create_gstreamer_pipeline(
-            capture_width=1280,
-            capture_height=720,
-            framerate=30,
+    camera = CSI_Camera()
+    camera.create_gstreamer_pipeline(
+            capture_width=CAPTURE_WIDTH,
+            capture_height=CAPTURE_HIEGHT,
+            framerate=FARMERATE,
             flip_method=0,
             display_height=DISPLAY_HEIGHT,
             display_width=DISPLAY_WIDTH,
     )
-    left_camera.open(left_camera.gstreamer_pipeline)
-    left_camera.start()
-    """
-    right_camera = CSI_Camera()
-    right_camera.create_gstreamer_pipeline(
-            sensor_id=0,
-            sensor_mode=SENSOR_MODE_720,
-            framerate=30,
-            flip_method=0,
-            display_height=DISPLAY_HEIGHT,
-            display_width=DISPLAY_WIDTH,
-    )
-    right_camera.open(right_camera.gstreamer_pipeline)
-    right_camera.start()
-    """
-    cv2.namedWindow("CSI Cameras", cv2.WINDOW_AUTOSIZE)
+    camera.open(camera.gstreamer_pipeline)
+    camera.start()
 
+    cv2.namedWindow("CSI Cameras", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("CSI Cameras",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
     if (
-        not left_camera.video_capture.isOpened()
-        #or not right_camera.video_capture.isOpened()
+        not camera.video_capture.isOpened()
     ):
         # Cameras did not open, or no camera attached
 
@@ -84,24 +81,21 @@ def start_cameras():
         SystemExit(0)
     try:
         # Start counting the number of frames read and displayed
-        left_camera.start_counting_fps()
+        camera.start_counting_fps()
         #right_camera.start_counting_fps()
         while cv2.getWindowProperty("CSI Cameras", 0) >= 0 :
-            left_image=read_camera(left_camera,show_fps)
-            #right_image=read_camera(right_camera,show_fps)
+            image=read_camera(camera,show_fps)
             # We place both images side by side to show in the window
-            #camera_images = np.hstack((left_image, right_image))
-            cv2.imshow("CSI Cameras", left_image)
-            left_camera.frames_displayed += 1
-            #right_camera.frames_displayed += 1
+            cv2.imshow("CSI Cameras", image)
+            camera.frames_displayed += 1
             # This also acts as a frame limiter
             # Stop the program on the ESC key
-            if (cv2.waitKey(1) & 0xFF) == 27:
+            if (cv2.waitKey(20) & 0xFF) == 27:
                 break   
 
     finally:
-        left_camera.stop()
-        left_camera.release()
+        camera.stop()
+        camera.release()
         #right_camera.stop()
         #right_camera.release()
     cv2.destroyAllWindows()
